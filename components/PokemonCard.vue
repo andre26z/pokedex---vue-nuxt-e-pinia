481dd55169cf9e78acb18ffa73b3d0b38b675f12
Andre Santos <andreaugustodossantos@gmail.com>
2024-04-13 14:02:56 -0300

    pokemon page - more feedback - backbutton

--- components/PokemonCard.vue
+++ components/PokemonCard.vue
@@ -1,41 +1,54 @@
 <template>
   <div
-    class="max-w-sm rounded overflow-hidden shadow-lg bg-white hover:bg-gray-100 transition-colors"
+    class="max-w-sm rounded overflow-hidden shadow-lg bg-white transition duration-200 ease-in-out transform hover:scale-105 hover:shadow-2xl cursor-pointer relative"
+    @click="handleClick"
   >
-    <div class="px-6 py-4">
-      <img
-        :src="pokemonDetails.image"
-        alt="Pokemon"
-        class="mx-auto h-auto max-w-full"
-        loading="lazy"
-      />
-      <div class="font-bold text-xl mb-6 capitalize text-center">
-        {{ pokemonDetails.name }}
-      </div>
-      <p class="font-bold text-sm mb-20 text-center">
-        Cód: {{ pokemonDetails.gameIndex }}
-      </p>
+    <div v-if="isLoading" class="absolute inset-0 bg-white bg-opacity-75 flex justify-center items-center">
+      <!-- Tailwind Spinner -->
+      <svg class="animate-spin h-5 w-5 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.313 2.686 6 6 6v-1.709z"></path>
+      </svg>
+    </div>
+    <div v-else>
+      <div class="px-6 py-4">
+        <img
+          :src="pokemon.image"
+          alt="Pokemon"
+          class="mx-auto h-auto max-w-full"
+          loading="lazy"
+        />
+        <div class="font-bold text-xl mb-6 capitalize text-center">
+          {{ pokemon.name }}
+        </div>
+        <p class="font-bold text-sm mb-20 text-center">
+          Cód: {{ pokemon.gameIndex }}
+        </p>
 
-      <div class="flex justify-center items-center space-x-2">
-        <span v-for="type in pokemonDetails.typeName" :key="type" class="mb-2">
-          <span
-            :class="[
-              typeColors[type],
-              'rounded-full px-4 py-1 text-sm font-semibold text-white uppercase',
-            ]"
-          >
-            {{ type }}
+        <div class="flex justify-center items-center space-x-2">
+          <span v-for="type in pokemon.typeName" :key="type" class="mb-2">
+            <span
+              :class="[
+                typeColors[type],
+                'rounded-full px-4 py-1 text-sm font-semibold text-white uppercase',
+              ]"
+            >
+              {{ type }}
+            </span>
           </span>
-        </span>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, watch } from "vue";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
 import { usePokemonStore } from "~/stores/usePokemonStore";
+import { defineProps } from "vue";
 
+const router = useRouter();
 const props = defineProps({
   pokemon: {
     type: Object,
@@ -43,11 +56,22 @@ const props = defineProps({
   },
 });
 
+const isLoading = ref(false);
+
+function handleClick() {
+  isLoading.value = true;
+  // Simulate a network delay
+  setTimeout(() => {
+    router.push(`/pokemon/${props.pokemon.name}`).finally(() => {
+      isLoading.value = false; // Reset loading state after navigation
+    });
+  }, 500); // Delay for half a second
+}
+
 const pokemonStore = usePokemonStore();
 const pokemonDetails = ref({});
 
 onMounted(async () => {
-  await pokemonStore.fetchPokemon();
   if (!pokemonStore.pokemonDetails[props.pokemon.url]) {
     await pokemonStore.fetchPokemonDetails(props.pokemon.url);
   }
@@ -87,4 +111,8 @@ const typeColors = {
   steel: "bg-indigo-800",
   unknown: "bg-emerald-950",
 };
+
+function navigateToPokemonDetail(name) {
+  router.push(`/pokemon/${name}`);
+}
 </script>
---
+++ pages/pokemon/[name].vue
@@ -0,0 +1,118 @@
+<template>
+  <Header />
+  <div v-if="pokemon" class="py-8 px-4 justify-center mx-auto lg:w-1/2 w-full">
+    <div class="bg-white shadow-lg rounded-lg overflow-hidden">
+      <div class="p-2">
+        <img
+          :src="pokemon.sprites.front_default"
+          alt="Front of Pokémon"
+          class="mx-auto w-64 h-auto"
+        />
+        <h1 class="text-3xl text-center mb-20 font-bold capitalize">
+          {{ pokemon.name }}
+        </h1>
+        <div class="mt-3 mb-3">
+          <ul class="flex justify-center items-center space-x-2">
+            <li v-for="type in pokemon.types" :key="type.slot">
+              <span
+                :class="[
+                  typeColors[type.type.name],
+                  'rounded-full px-4 py-1 text-sm font-semibold text-white uppercase',
+                ]"
+              >
+                {{ type.type.name }}
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="bg-white shadow-lg rounded-lg overflow-hidden mt-4">
+      <div class="p-4">
+        <h1 class="text-lg justify-center text-center font-bold">
+          Habilidades:
+        </h1>
+        <ul>
+          <li
+            v-for="ability in pokemon.abilities"
+            :key="ability.ability.name"
+            class="px-3 py-4 text-sm font-semibold text-gray-700 mr-2"
+          >
+            {{ ability.ability.name }}
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- Back Button -->
+    <div class="text-center mt-6">
+      <button
+        @click="goBack"
+        class="bg-white-500 hover:text-cyan-800 text-cyan-600 font-bold py-2 px-4 rounded"
+      >
+        Voltar
+      </button>
+    </div>
+  </div>
+  <div v-else class="text-center flex justify-center mt-30">
+    <p>Carregando...</p>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  data() {
+    return {
+      pokemon: null,
+    };
+  },
+  created() {
+    this.fetchPokemon();
+  },
+  methods: {
+    async fetchPokemon() {
+      try {
+        const res = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${this.$route.params.name}`
+        );
+        this.pokemon = await res.json();
+      } catch (error) {
+        console.error("Error fetching Pokémon data:", error);
+        this.pokemon = null;
+      }
+    },
+    goBack() {
+      this.$router.push("/");
+    },
+  },
+  computed: {
+    typeColors() {
+      return {
+        water: "bg-blue-500",
+        grass: "bg-green-500",
+        normal: "bg-orange-950",
+        bug: "bg-lime-300",
+        dark: "bg-gray-800",
+        dragon: "bg-emerald-800",
+        electric: "bg-yellow-400",
+        fairy: "bg-pink-500",
+        fire: "bg-red-500",
+        flying: "bg-sky-300",
+        ghost: "bg-gray-500",
+        ground: "bg-amber-600",
+        ice: "bg-blue-300",
+        poison: "bg-purple-700",
+        psychic: "bg-pink-300",
+        fighting: "bg-red-300",
+        rock: "bg-amber-700",
+        shadow: "bg-coolGray-600",
+        steel: "bg-indigo-800",
+        unknown: "bg-emerald-950",
+      };
+    },
+  },
+};
+</script>
